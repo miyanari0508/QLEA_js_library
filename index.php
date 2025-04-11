cd3ebac6df62c1620540c9d5ae55a38dc6fb2ae1
miyanari0508 <miyanari@qlea.co.jp>
2025-04-11 13:04:58 +0900

    ベースファイル作成

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include(dirname(__FILE__) . '/config/config.php'); ?>
+<?php
+$title = '';
+$page = '';
+$og_type = 'website';
+include(dirname(__FILE__) . '/inc/head.php');
+?>
+</head>
+<!-- ____________________//head____________________ -->
+
+<body id="pageTop">
+  <!-- ______main______________//header____________________ -->
+  <main id="mainWrap" class="active">
+    <section>
+      <!-- ____________________content____________________ -->
+      <div class="com-content">
+        
+      </div>
+
+    </section>
+    <!-- ____________________//content__________________ -->
+  </main>
+
+
+
+  <!-- ____________________// footer -->
+  <!-- ____________________script____________________ -->
+  <!-- __________________//script____________________ -->
+</body>
+
+</html>
